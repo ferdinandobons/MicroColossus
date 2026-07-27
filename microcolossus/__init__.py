2af4f5ef16dbdbc357b37ea8874fb3ce43a2f921
Ferdinando Bonsegna <1bonsegnaferdinando@gmail.com>
2026-07-27 20:29:48 +0200

    Export bounded optimizer APIs

--- microcolossus/__init__.py
+++ microcolossus/__init__.py
@@ -10,21 +10,30 @@ from .bounded_forward import (
     WorkingSetExceededError,
     run_bounded_forward,
 )
+from .bounded_optimizer import (
+    BoundedOptimizerResult,
+    OptimizerWorkingSetExceededError,
+    run_bounded_optimizer_step,
+)
 from .config import ExperimentConfig, HardwareBudget, ModelConfig, TrainingConfig
 from .model import DecoderOnlyTransformer
 from .planner import MemoryPlan, build_static_plan
+from .step_bundle import StepBundleStore
 from .storage import StoreLimits, VersionedTensorStore
 from .storage_training import StorageBackedStepResult, run_observable_storage_step
 
 __all__ = [
     "BoundedBackwardResult",
     "BoundedForwardResult",
+    "BoundedOptimizerResult",
     "DecoderOnlyTransformer",
     "ExperimentConfig",
     "GradientWorkingSetExceededError",
     "HardwareBudget",
     "MemoryPlan",
     "ModelConfig",
+    "OptimizerWorkingSetExceededError",
+    "StepBundleStore",
     "StorageBackedStepResult",
     "StoreLimits",
     "TrainingConfig",
@@ -33,7 +42,8 @@ __all__ = [
     "build_static_plan",
     "run_bounded_backward",
     "run_bounded_forward",
+    "run_bounded_optimizer_step",
     "run_observable_storage_step",
 ]
 
-__version__ = "0.7.0"
+__version__ = "0.8.0"
