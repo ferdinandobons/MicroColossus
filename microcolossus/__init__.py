1786916f6d7bc4cf96d62410b0f95d0fca4b270e
Ferdinando Bonsegna <1bonsegnaferdinando@gmail.com>
2026-07-27 17:57:21 +0200

    Export bounded backward runtime

--- microcolossus/__init__.py
+++ microcolossus/__init__.py
@@ -1,5 +1,10 @@
 """MicroColossus public package."""
 
+from .bounded_backward import (
+    BoundedBackwardResult,
+    GradientWorkingSetExceededError,
+    run_bounded_backward,
+)
 from .bounded_forward import (
     BoundedForwardResult,
     WorkingSetExceededError,
@@ -12,9 +17,11 @@ from .storage import StoreLimits, VersionedTensorStore
 from .storage_training import StorageBackedStepResult, run_observable_storage_step
 
 __all__ = [
+    "BoundedBackwardResult",
     "BoundedForwardResult",
     "DecoderOnlyTransformer",
     "ExperimentConfig",
+    "GradientWorkingSetExceededError",
     "HardwareBudget",
     "MemoryPlan",
     "ModelConfig",
@@ -24,8 +31,9 @@ __all__ = [
     "VersionedTensorStore",
     "WorkingSetExceededError",
     "build_static_plan",
+    "run_bounded_backward",
     "run_bounded_forward",
     "run_observable_storage_step",
 ]
 
-__version__ = "0.6.0"
+__version__ = "0.7.0"
